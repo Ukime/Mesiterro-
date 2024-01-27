0a20d493d195d21d0accc702ae2f662eb13ecf33
EC2 Default User <ec2-user@ip-172-31-27-15.ap-northeast-1.compute.internal>
2024-01-27 12:06:35 +0000

    [Finish] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  resources :post_images, only: [:new, :index, :show]
+
   devise_for :users
+
   root to: "homes#top"
+
   get '/homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
